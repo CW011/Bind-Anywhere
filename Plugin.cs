c4b95a3a6325924c7baa456cc5d0c50ed6bc25bb
CW011 <122088905+CW011@users.noreply.github.com>
2025-05-10 23:46:54 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Spt.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bind_Anywhere
+{
+    [BepInPlugin("com.bepinex.plugins.bindanywhere", "Bind Anywhere", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin test
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded! Hi!")
+        }
+    }
+}
+
+// test
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bind Anywhere")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bind Anywhere")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c18bcd4-f521-44e5-84d4-9645e0110687")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
